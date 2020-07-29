8a097171924275f0f4270d6dc2a61feb57dda849
BartTheScrivner <dframe22@gmail.com>
2020-07-29 16:02:49 -0400

    done with posting entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,28 @@
 class EntriesController < ApplicationController
+
+    def create
+        entry = Entry.create(entry_params)
+        render json: entry
+    end 
+
+    def edit
+        @entry = Entry.find(params[:id])
+    end 
+
+    def update
+        @entry = Entry.find(params[:id])
+        @entry.update(entry_params)
+    end 
+
+    def destroy
+        @entry = Entry.find(params[:id])
+        @entry.destroy 
+    end 
+
+    private 
+
+    def entry_params
+        params.require(:entry).permit(:mood, :narrative, :user_id)
+    end 
+
 end
