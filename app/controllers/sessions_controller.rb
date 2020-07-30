9eac82aad862c1412070d79a717836fa45a6188a
BartTheScrivner <dframe22@gmail.com>
2020-07-29 22:15:14 -0400

    fixed inclusion of entries in user object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:_json])
-    render json: user, include: [:affirmations, :friendships]
+    render json: user, include: [:affirmations, :friendships, :entries]
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    render json: user, include: [:affirmations, :friendships, :friend_requests, :entries]
+    byebug
+    render json: user, include: [:affirmations, :friendships]
   end
 end
 
