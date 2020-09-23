2204baace336e4d28b09aa0a7dba1b6a97544011
BartTheScrivner <dframe22@gmail.com>
2020-09-23 14:05:47 -0400

    removed byebug

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,6 @@
 class FriendshipsController < ApplicationController
 
   def destroy
-    byebug
     friendship = Friendship.find(params[:id])
     friendship.destroy
     render json: {:message =>"Successful"}
